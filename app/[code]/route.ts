50a4121fd502667906bf08da33a155b7bd456a31
Max Chmelicek <max.chmelicek@gmail.com>
2026-09-10 14:37:14 +0200

    feat: functionality is working

--- app/[code]/route.ts
+++ app/[code]/route.ts
@@ -1,1 +1,20 @@
 import { supabase } from "@/lib/supabase";
+import { NextRequest, NextResponse } from "next/server";
+
+
+
+export async function GET(request: NextRequest, { params }: { params: Promise<{ code: string }>}) {
+    const { code } = await params;
+
+    const { data, error } = await supabase
+    .from('links')
+    .select()
+    .eq('code', code)
+    .single();
+
+    if (error) {
+        return NextResponse.redirect(`http://${process.env.PROJECT_DOMAIN}/not-found`);
+    }
+
+    return NextResponse.redirect(data.original_url);
+}
